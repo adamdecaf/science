c0d406a00792615ea19c867cb9d40dcff16a2336
Adam Shannon <adam.shannon@banno.com>
2015-02-03 12:13:26 -0600

    generate new random for each experiment strategy

--- core/src/main/scala/ExperimentStrategy.scala
+++ core/src/main/scala/ExperimentStrategy.scala
@@ -1,14 +1,14 @@
 package org.adamdecaf.scientist
+import scala.util.Random
 
 trait ExperimentStrategy[T] {
   def experiment(candidate: => T): Option[T]
 }
 
 object ExperimentStrategy {
-  import scala.util.Random
-  private[this] val random = new Random()
-
   def default[T]: ExperimentStrategy[T] = new ExperimentStrategy[T] {
+    private[this] val random = new Random()
+
     def experiment(candidate: => T): Option[T] =
       if (random.nextBoolean()) {
         Some(candidate)
