b75c14e830cec22eced75a04c10c668e2d682cce
Adam Shannon <adam@ashannon.us>
2015-06-25 16:16:06 -0500

    Adding a test for exceptions thrown during experiments to be under a different meter.

--- metrics/src/test/scala/MetricsExperimentSpec.scala
+++ metrics/src/test/scala/MetricsExperimentSpec.scala
@@ -10,14 +10,28 @@ object MetricsExperimentSpec extends Specification {
     (1 to 10) foreach { _ =>
       always(control(), experiment())
     }
+
     storage.getAllControlResults() must have size(10)
 
-    val meter: Meter = registry.meter("test-metrics-experiment.control")
-    meter.getCount() must be_==(10)
+    val meter1: Meter = registry.meter("test-metrics-experiment.control")
+    val meter2: Meter = registry.meter("test-metrics-experiment.experiment")
+    val meter3: Meter = registry.meter("test-metrics-experiment.exceptions-during-experiments")
+
+    meter1.getCount() must be_==(10)
+    meter2.getCount() must be_==(10)
+    meter3.getCount() must be_==(0)
+  }
+
+  "mark a failure during experiments" in new context {
+    always(control(), failing())
+    storage.getAllControlResults() must beEmpty
+    val meter: Meter = registry.meter("test-metrics-experiment.exceptions-during-experiments")
+    meter.getCount() must be_==(1)
   }
 
   def control(): String = "control"
   def experiment(): String = "experiment"
+  def failing(): String = throw new Exception("nope")
 
   trait context extends Scope {
     val registry = new MetricRegistry()
